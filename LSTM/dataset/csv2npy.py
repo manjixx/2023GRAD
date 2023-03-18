0182a5c4be7b56a9acec500787e7edecabddfc99
manjixx <xiaochuiwei@gmail.com>
2023-03-18 16:26:04 +0800

    daily update

--- LSTM/dataset/csv2npy.py
+++ LSTM/dataset/csv2npy.py
@@ -15,31 +15,90 @@ import matplotlib.pyplot as plt
 
 
 def deal_2018():
+
     # 数据清洗
     df = pd.read_csv('../../dataset/2018.csv').dropna(axis=0, how='any', inplace=False)
     data = df[(df.time != '8:50:00') & (df.time != '14:20:00') & (df.time != '18:00:00')]
     data = data.drop(data.index[(data.no == 3) & (data.date == '2018/7/16')])
     data = data.drop(data.index[(data.no == 6) & (data.date == '2018/7/16')])
+    data.loc[(data['time'] == '9:00:00'), 'time'] = '09:00:00'
+    data.loc[(data['time'] == '9:30:00'), 'time'] = '09:30:00'
 
     # 标签数据
     data.loc[(data[y_feature] > 0.5), y_feature] = 2
     data.loc[((-0.5 <= data[y_feature]) & (data[y_feature] <= 0.5)), y_feature] = 1
     data.loc[(data[y_feature] < -0.5), y_feature] = 0
-    label1 = data[y_feature].reset_index(drop=True).values.astype(int)
+    label = data[y_feature].reset_index(drop=True).values.astype(int)
     # 人员特征
 
-    body1 = data[body_feature].reset_index(drop=True)
-    gender1 = data['gender'].reset_index(drop=True)
+    body = data[body_feature].reset_index(drop=True)
+    gender = data['gender'].reset_index(drop=True)
 
     # 环境特征
     va = []
     for i in range(0, data.shape[0]):
         va.append(0.7 * round(random.random(), 1))
     va = pd.DataFrame({'va': va})
-    env1 = data[env_feature].reset_index(drop=True)
-    env1 = pd.concat([env1, va], axis=1)
+    env = data[env_feature].reset_index(drop=True)
+    env = pd.concat([env, va], axis=1)
+    return body, gender, env, label
 
-    return body1, gender1, env1, label1
+
+def deal_2019_summer():
+    df = pd.read_csv('../../dataset/2019_summer_clean.csv').dropna(axis=0, how='any', inplace=False)
+    data = df.drop(df.index[(df.time == '12:30:00') | (df.time == '13:00:00') |
+                            (df.time == '13:30:00') | (df.time == '14:00:00') |
+                            (df.time == '18:00:00')])
+    data = data.drop(data.index[(data.no == 9) & (data.date == '2019/7/29')])
+    data.loc[(data['time'] == '9:00:00'), 'time'] = '09:00:00'
+    data.loc[(data['time'] == '9:30:00'), 'time'] = '09:30:00'
+    data = data.sort_values(by=['no', 'date', 'time'], axis=0, ascending=True, inplace=False)
+
+    # 标签数据
+    data.loc[(data[y_feature] > 0.5), y_feature] = 2
+    data.loc[((-0.5 <= data[y_feature]) & (data[y_feature] <= 0.5)), y_feature] = 1
+    data.loc[(data[y_feature] < -0.5), y_feature] = 0
+    label = data[y_feature].reset_index(drop=True).values.astype(int)
+
+    # 人员特征
+    body = data[body_feature].reset_index(drop=True)
+    gender = data['gender'].reset_index(drop=True)
+
+    # 环境特征
+    va = []
+    for i in range(0, data.shape[0]):
+        va.append(0.7 * round(random.random(), 1))
+    va = pd.DataFrame({'va': va})
+    env = data[env_feature].reset_index(drop=True)
+    env = pd.concat([env, va], axis=1)
+    return body, gender, env, label
+
+
+def deal_2019_winter():
+    df = pd.read_csv('../../dataset/2019_winter.csv').dropna(axis=0, how='any', inplace=False)
+    data = df.drop(df.index[(df.time == '8:40:00') | (df.time == '8:50:00') | (df.time == '14:10:00')])
+    data.loc[(data['time'] == '9:00:00'), 'time'] = '09:00:00'
+    data.loc[(data['time'] == '9:30:00'), 'time'] = '09:30:00'
+    data = data.sort_values(by=['no', 'date', 'time'], axis=0, ascending=True, inplace=False)
+
+    # 标签数据
+    data.loc[(data[y_feature] > 0.5), y_feature] = 2
+    data.loc[((-0.5 <= data[y_feature]) & (data[y_feature] <= 0.5)), y_feature] = 1
+    data.loc[(data[y_feature] < -0.5), y_feature] = 0
+    label = data[y_feature].reset_index(drop=True).values.astype(int)
+
+    # 人员特征
+    body = data[body_feature].reset_index(drop=True)
+    gender = data['gender'].reset_index(drop=True)
+
+    # 环境特征
+    va = data['va'].reset_index(drop=True)
+    for i in range(0, data.shape[0]):
+        va.append(0.7 * round(random.random(), 1))
+    va = pd.DataFrame({'va': va})
+    env = data[env_feature].reset_index(drop=True)
+    env = pd.concat([env, va], axis=1)
+    return body, gender, env, label
 
 
 if __name__ == '__main__':
@@ -51,45 +110,66 @@ if __name__ == '__main__':
     y_feature = 'thermal sensation'
 
     '''2018 summer'''
-    body1, gender1, env1, label1 = deal_2018()
-
+    # body1, gender1, env1, label1 = deal_2018()
 
     ''' 2019 summer'''
-    9 7-29
-
-
-    df = pd.read_csv('../../dataset/2019_summer_clean.csv').dropna(axis=0, how='any', inplace=False)
-    data = df[(df.time != '8:50:00') & (df.time != '14:20:00') & (df.time != '18:00:00')]
-    data = data.drop(data.index[(data.no == 3) & (data.date == '2018/7/16')])
-    data = data.drop(data.index[(data.no == 6) & (data.date == '2018/7/16')])
-
-    # 标签数据
-    data.loc[(data[y_feature] > 0.5), y_feature] = 2
-    data.loc[((-0.5 <= data[y_feature]) & (data[y_feature] <= 0.5)), y_feature] = 1
-    data.loc[(data[y_feature] < -0.5), y_feature] = 0
-    label1 = data[y_feature].reset_index(drop=True).values.astype(int)
-    # 人员特征
-
-    body1 = data[body_feature].reset_index(drop=True)
-    gender1 = data['gender'].reset_index(drop=True)
-
-    # 环境特征
-    va = []
-    for i in range(0, data.shape[0]):
-        va.append(0.7 * round(random.random(), 1))
-    va = pd.DataFrame({'va': va})
-    env1 = data[env_feature].reset_index(drop=True)
-    env1 = pd.concat([env1, va], axis=1)
-
-    df = pd.read_csv('../../dataset/2019_summer.csv').dropna(axis=0, how='any', inplace=False)
-    data = df[(df.time != '8:50:00') & (df.time != '14:20:00') & (df.time != '18:00:00')]
-    # df_2021 = pd.read_csv('../../dataset/2021.csv', encoding='gbk').dropna(axis=0, how='any', inplace=False)
-    # data = df_2021.loc[df_2021['season'] == 'summer']
+    # body2, gender2, env2, label2 = deal_2019_summer()
 
     ''' 2019 winter'''
 
+    # body3, gender3, env3, label3 = deal_2019_summer()
 
     ''' 2021 '''
+    df = pd.read_csv('../../dataset/2021.csv').dropna(axis=0, how='any', inplace=False)
+    data = df.loc[(df['season'] == 'summer')].reset_index(drop=False)
+    data = data.drop(data.index[(data.time > '17:30:00')])
+    print(data)
+
+    no = sorted(np.array(df['no'].unique()))
+
+    for k in np.array(no):
+        print(k)
+        print(data[data.no == k].shape[0])
+    # for k in np.array(data[data.no == 8]):
+    #     print(k)
+
+    # data = df.drop(df.index[(df.time == '12:30:00') | (df.time == '13:00:00') |
+    #                         (df.time == '13:30:00') | (df.time == '14:00:00') |
+    #                         (df.time == '18:00:00')])
+    # data = data.drop(data.index[(data.no == 9) & (data.date == '2019/7/29')])
+    # data.loc[(data['time'] == '9:00:00'), 'time'] = '09:00:00'
+    # data.loc[(data['time'] == '9:30:00'), 'time'] = '09:30:00'
+
+    # no = sorted(np.array(df['no'].unique()))
+    #
+    # for k in np.array(no):
+    #     print(k)
+    #     print(data[data.no == k].shape[0])
+    #
+
+
+        # print(gender3)
+        # print(body3)
+        # print(env3)
+        # print(label3)
+
+    # 标签数据
+    # data.loc[(data[y_feature] > 0.5), y_feature] = 2
+    # data.loc[((-0.5 <= data[y_feature]) & (data[y_feature] <= 0.5)), y_feature] = 1
+    # data.loc[(data[y_feature] < -0.5), y_feature] = 0
+    # label1 = data[y_feature].reset_index(drop=True).values.astype(int)
+    # # 人员特征
+    #
+    # body1 = data[body_feature].reset_index(drop=True)
+    # gender1 = data['gender'].reset_index(drop=True)
+    #
+    # # 环境特征
+    # va = []
+    # for i in range(0, data.shape[0]):
+    #     va.append(0.7 * round(random.random(), 1))
+    # va = pd.DataFrame({'va': va})
+    # env1 = data[env_feature].reset_index(drop=True)
+    # env1 = pd.concat([env1, va], axis=1)
 
 
     # '''plot'''
